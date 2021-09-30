534ea3bfff47443185b00a95650856ab44421352
Rodrigo Faria <rodrigo.faria@blueorange.digital>
2021-09-29 23:07:04 -0300

    missing class

---
+++ oam/SubspacesGeneration.py
@@ -0,0 +1,46 @@
+from itertools import combinations
+
+ 
+
+class SubspacesGeneration:
+
+ 
+
+    def __init__(self):
+
+        pass
+
+ 
+
+    def generateSubspaces(self, generationMethod, **kwargs):
+
+ 
+
+        if generationMethod=="SimpleCombination":
+
+            return self.generateSubspacesBySimpleCombination(kwargs["attributesList"],kwargs["minItemsPerSubspace"],kwargs["maxItemsPerSubspace"])
+
+            
+
+        else:
+
+            raise Exception('Metodo nao encontrado')
+
+ 
+
+    def generateSubspacesBySimpleCombination(self, attributesList, minItemsPerSubspace, maxItemsPerSubspace):
+
+        
+
+        subspacesList = []
+
+ 
+
+        for itBoundary in range(minItemsPerSubspace, maxItemsPerSubspace):
+
+            subspacesList = subspacesList + [list(x) for x in combinations(attributesList, itBoundary)]
+
+        
+
+        return subspacesList
+
--- oam/ipath.py
+++
@@ -1,2 +0,0 @@
-class IPath:
-    print('ola')
