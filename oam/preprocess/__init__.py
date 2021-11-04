842360c95ce5e2c71eea6a30a4b764af167175c1
Rodrigo Faria <rodrigo.faria@blueorange.digital>
2021-11-04 13:12:49 -0300

    adding weights to normalization

--- oam/preprocess/__init__.py
+++ oam/preprocess/__init__.py
@@ -1,4 +1,12 @@
-def normalize(df, min, max):
+import pandas as pd
+
+
+def normalize(df: pd.DataFrame, min: int, max: int, weights: dict = None):
     normalized_df = (df-df.min())/(df.max()-df.min())
     X_scaled = normalized_df * (max - min) + min
+
+    if weights:
+        for column, weight in weights.items():
+            df[column] = df[column] * weight
+
     return X_scaled
